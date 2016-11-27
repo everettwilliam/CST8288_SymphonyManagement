e5a9b038d146d3704e44a7630e8cc0a469e217f3
everettwilliam <hold0052@algonquinlive.com>
2016-11-26 19:27:13 -0500

    updated run.bat

--- batch/run.bat
+++ batch/run.bat
@@ -2,7 +2,7 @@ ECHO
 
 cd .. 
 
-java -classpath "C:\Users\eberhard\Desktop\Term 3\CST8288 - OOP with Design Patterns\CST8288 Projects\Project 1\CST8288_SymphonyManagement";"C:\Program Files\Java\junit-4.10.jar" symphony.test.AllTests
+java -classpath "./junit-4.10.jar";"." symphony.test.AllTests
 
 PAUSE
 
